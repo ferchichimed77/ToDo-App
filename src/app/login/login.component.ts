8dfc163bcaf746aa6e8bc030415229e7cb75651d
AhmedMaghraoui <94858065+AhmedMaghraoui@users.noreply.github.com>
2021-12-09 22:55:57 +0100

    bug login fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {}
   //Appel du service de login
   onLogin(loginForm: NgForm) {
-    this.userService.loginUser(loginForm.toStrong());
+    this.userService.loginUser(loginForm.value);
   }
   //redirection vers la page register
   onRegister() {
